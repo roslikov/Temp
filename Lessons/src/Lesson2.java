4d38c345a6184b23aadf64e85c74de54d1ab844c
roslikov <roslikov.aleksandr2016@yandex.ru>
2020-02-14 14:49:06 +0300

    update

--- Lessons/src/Lesson2.java
+++ Lessons/src/Lesson2.java
@@ -1,6 +1,16 @@
 public class Lesson2 {
 
     public static void main(String[] args) {
-
+        int SecIn = 4018385;
+        int sec = SecIn % 60;
+        int m = (SecIn - sec) / 60;
+        int min = m % 60;
+        int h = (m - min) /  60;
+        int hour = h % 60;
+        int d = (h - hour) / 24;
+        int day = d % 30;
+        int week = (d - day) /7;
+        System.out.println(week + " недель " + day + " дней " + hour +
+                " часов " + min + " минут " + sec + " секунд.");
     }
 }
