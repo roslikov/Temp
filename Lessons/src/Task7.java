a0bb8ef5abb2b29a9e3bf3d31a662aee1eade129
roslikov <roslikov.aleksandr2016@yandex.ru>
2020-02-17 13:46:17 +0300

    Finish_Update

--- Lessons/src/Task7.java
+++ Lessons/src/Task7.java
@@ -16,17 +16,29 @@ public class Task7 {
             int wThoHouse = wFirstHouse + wSecondHouse;
             int hwThoHouse = hFirstHouse + wSecondHouse;
             int whThoHouse = wFirstHouse + hSecondHouse;
+        int hMax = Math.max(hFirstHouse, hSecondHouse);
+        int wMax = Math.max(wFirstHouse, wSecondHouse);
+        int hwMax = Math.max(hFirstHouse, wSecondHouse);
+        int whMax = Math.max(wFirstHouse, hSecondHouse);
 
             /*Проверяем поместятся ли дома до одной из сторон участка*/
-            if (hPlot >= hThoHouse && wPlot >= wThoHouse){
+            if (hPlot >= hThoHouse && wPlot >= wMax){
                 System.out.println("Все ГУД!");
-            } else if (hPlot >= wThoHouse && wPlot >= hThoHouse){
+            } else if (hPlot >= wThoHouse && wPlot >= hMax){
                 System.out.println("Все ГУД!");
-            } else if (hPlot >= hwThoHouse && wPlot >= whThoHouse){
+            } else if (hPlot >= hwThoHouse && wPlot >= whMax){
                 System.out.println("Все ГУД!");
-            } else if (hPlot >= whThoHouse && wPlot >= hwThoHouse){
+            } else if (hPlot >= whThoHouse && wPlot >= hwMax){
                 System.out.println("Все ГУД!");
-            } else {
+            } else if (wPlot >= hThoHouse && hPlot >= wMax) {
+                System.out.println("Все ГУД!");
+            } else if (wPlot >= wThoHouse && hPlot >= hMax){
+                System.out.println("Все ГУД!");
+            } else if (wPlot >= hwThoHouse && hPlot >= whMax){
+                System.out.println("Все ГУД!");
+            } else if (wPlot >= whThoHouse && hPlot >= hwMax){
+                System.out.println("Все ГУД!");
+            } else  {
                 System.out.println("Дома на учаток НЕ поместятся!");
             }
     }
