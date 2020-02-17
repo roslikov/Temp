a684e91236054ee5e4eaf42c67313dab6865455f
roslikov <roslikov.aleksandr2016@yandex.ru>
2020-02-17 19:04:45 +0300

    Update

--- Lessons/src/Task5.java
+++ Lessons/src/Task5.java
@@ -4,13 +4,15 @@ public class Task5 {
     Вывести это число, добавив к нему слово "рублей" в првильном падеже.*/
 
     public static void main(String[] args) {
-        int Money = 764;            //Сумма денег
+        int Money = 1;            //Сумма денег
 
         if (Money < 0) {            //Если сумма меньше 0
             System.out.println("Должен Васе " + (Money * -1) + " денег... Печаль... Беда...");
         } else if (Money == 0) {    //Если сумма равна 0
             System.out.println("Денег нет совсем, но вы держитесь.");
-        } else if (Money % 10 == 0 || Money % 10 == 5
+        } else if (Money >= 11 && Money <= 19) {
+            System.out.println(Money + " рублей");
+        } else if ( Money % 10 == 0 || Money % 10 == 5
            || Money % 10 == 6 || Money % 10 == 7
            || Money % 10 == 8 || Money % 10 == 9){
             System.out.println(Money + " рублей.");
@@ -19,7 +21,6 @@ public class Task5 {
         } else if (Money % 10 == 2 || Money % 10 == 3 || Money % 10 == 4) {
             System.out.println(Money + " рубля.");
         }
-
 
     }
 }
