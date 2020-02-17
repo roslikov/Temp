d90d49cd2d673639e4ed2f6069bff4411feb669f
roslikov <roslikov.aleksandr2016@yandex.ru>
2020-02-17 05:46:05 +0300

    Перемудрил с инкрементом. Поправил

--- Lessons/src/Task6.java
+++ Lessons/src/Task6.java
@@ -4,8 +4,8 @@ public class Task6 {
     Ввести в виде трех чисел дату следующего дня.*/
 
     public static void main(String[] args) {
-        int day = 30;
-        int month = 11;
+        int day = 28;
+        int month = 2;
         int year = 2020;
         /*Проверка на корректность даты*/
         if (day < 1 || day > 31) {
@@ -19,23 +19,23 @@ public class Task6 {
         } else if (day == 31 && (month == 4 || month == 6 || month == 9 || month == 11)){
             System.out.println("Такой даты не существует! Измените день!");
         } else {
-            day = day++;
+            day = day + 1;
             if (day >= 30 && month == 2 && year % 4 == 0){
                 day = 1;
-                month = month++;
+                month = month + 1;
             } else if (day >= 29 && month == 2 && year % 4 != 0){
                 day = 1;
-                month = month++;
+                month = month + 1;
             } else if (day == 31 && (month == 4 || month == 6 || month == 9 || month == 11)){
                 day = 1;
-                month = month++;
+                month = month + 1;
             } else if (day > 31 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10)){
                 day = 1;
-                month = month++;
+                month = month + 1;
             } else  if (day > 31 && month == 12){
                 day = 1;
                 month = 1;
-                year = year++;
+                year = year + 1;
             }
             System.out.println("Следующая дата: " + day + "." + month + "." + year);
         }
