ca27c2fdcf34623e105bf960748dd265171a884f
roslikov <roslikov.aleksandr2016@yandex.ru>
2020-02-18 19:25:43 +0300

    Update

--- Lessons/src/Task8.java
+++ Lessons/src/Task8.java
@@ -3,17 +3,21 @@ public class Task8 {
     /*Посчитать факториал числа X*/
 
     public static void main(String[] args) {
+
+        int number = 11;
         int sum = 1;
         int i = 1;
-        int x = 11;
-        if (x >= 17 || x <= 0) {
+
+        if (number >= 17 || number <= 0) {
+                //Больше 16 - нужно использовать тип Long
             System.out.println("Введите целое положительное число X до 16 включительно.");
         } else {
-            while (i <= x) {
+                //Умножаем цифры в цикле while
+            while (i <= number) {
                 sum *= i;
                 i++;
             }
-            System.out.println("Факториал числа " + x + " = " + sum);
+            System.out.println("Факториал числа " + number + " = " + sum);
         }
 
     }
