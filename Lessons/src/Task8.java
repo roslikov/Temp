630973e5e4ebcca7020bb5f92c86190715ce4116
roslikov <roslikov.aleksandr2016@yandex.ru>
2020-02-18 08:09:14 +0300

    Update

--- Lessons/src/Task8.java
+++ Lessons/src/Task8.java
@@ -1,13 +1,21 @@
 public class Task8 {
+
+    /*Посчитать факториал числа X*/
+
     public static void main(String[] args) {
         int sum = 1;
         int i = 1;
-        int x = 10;
-        while (i <= x) {
-            sum *= i;
-            i++;
+        int x = 11;
+        if (x >= 17 || x <= 0) {
+            System.out.println("Введите целое положительное число X до 16 включительно.");
+        } else {
+            while (i <= x) {
+                sum *= i;
+                i++;
+            }
+            System.out.println("Факториал числа " + x + " = " + sum);
         }
-        System.out.println("Факториал числа " + x + " = " + sum);
+
     }
 
 }
