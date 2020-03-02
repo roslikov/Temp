b0590c6cbd802515fafa09ad5b15a6093ff3fa94
roslikov <roslikov.aleksandr2016@yandex.ru>
2020-03-02 14:38:27 +0300

    Modified Task20

--- Lessons/src/Lesson5/Task20.java
+++ Lessons/src/Lesson5/Task20.java
@@ -4,29 +4,28 @@ public class Task20 {
 
     /*Имеется строка с текстом. Вывести текст, составленный из последних букв всех слов.*/
 
-    public static String string = "   Привет    мир, это. задание   Требует умение работы со строками";
+    public static String string = "   Привет и но    мир, это. задание   Требует умение работы со строками";
 
     public static void main(String[] args) {
 
-        System.out.println(lastLettrersOfWord());
-
+        System.out.println(lastLettersWords());
     }
 
-    public static String lastLettrersOfWord() {
-        string = string.trim();
-        String[] arrString = string.split(" ");
+    public static StringBuilder lastLettersWords(){
+
         StringBuilder newString = new StringBuilder();
-        for (String word : arrString) {
-            for (int i = word.length() - 1; i > 0; i--) {
-                if (Character.isLetter(word.charAt(i))) {
-                    newString.append(word.charAt(i));
-                    break;
+        String[] arrayOfWords = string.split(" ");
+
+        for (String arrayOfWord : arrayOfWords) {
+
+            if (arrayOfWord.length() > 0)
+
+                if (Character.isLetter(arrayOfWord.charAt(arrayOfWord.length() - 1))) {
+                    newString.append(arrayOfWord.charAt(arrayOfWord.length() - 1));
+                } else {
+                    newString.append(arrayOfWord.charAt(arrayOfWord.length() - 2));
                 }
-            }
-            
         }
-        return newString.toString();
-
+        return newString;
     }
-
 }
