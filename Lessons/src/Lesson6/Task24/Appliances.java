bca0459c5586ce87429bc176ecea6e154ddae130
roslikov <roslikov.aleksandr2016@yandex.ru>
2020-03-04 21:38:03 +0300

    Create Task24

---
+++ Lessons/src/Lesson6/Task24/Appliances.java
@@ -0,0 +1,25 @@
+package Lesson6.Task24;
+
+public class Appliances {
+    String manufacturer;
+    String countryManufacturer;
+    int power;
+    boolean pluggedInPower;
+
+    @Override
+    public String toString() {
+        return "Appliances{" +
+                "manufacturer='" + manufacturer + '\'' +
+                ", countryManufacturer='" + countryManufacturer + '\'' +
+                ", power=" + power +
+                ", pluggedInPower=" + pluggedInPower +
+                '}';
+    }
+
+    public Appliances(String manufacturer, String countryManufacturer, int power, boolean pluggedInPower){
+        this.manufacturer = manufacturer;
+        this.countryManufacturer = countryManufacturer;
+        this.power = power;
+        this.pluggedInPower = pluggedInPower;
+    }
+}
