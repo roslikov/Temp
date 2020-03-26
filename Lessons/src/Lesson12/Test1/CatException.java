d1d2a4ef408fd5284953357434e97c7232a60371
Roslikov Aleksandr <sanek20062006@mail.ru>
2020-03-26 08:53:37 +0300

    Create Task30

---
+++ Lessons/src/Lesson12/Test1/CatException.java
@@ -0,0 +1,8 @@
+package Lesson12.Test1;
+
+public class CatException extends RuntimeException {
+
+    public CatException(final String message) {
+        super(message);
+    }
+}
---
+++ Lessons/src/Lesson12/Test1/CatExtraException.java
@@ -0,0 +1,7 @@
+package Lesson12.Test1;
+
+public class CatExtraException extends CatException {
+    public CatExtraException(final String message) {
+        super(message);
+    }
+}
