ea8a09c939f8e2c9ac26f7b6ca6db16b6c57fb04
Roslikov Aleksandr <sanek20062006@mail.ru>
2020-03-28 12:40:12 +0300

    Create Task33

---
+++ Lessons/src/Lesson14/Task33/Main.java
@@ -0,0 +1,10 @@
+package Lesson14.Task33;
+
+/*Вывести список файлов и каталогов выбранного каталога на диске. Файлы
+и каталоги должны быть распечатаны отдельно.*/
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
