ab3d133398b715dec6c4b0d4cc8ea19a1b968135
Roslikov Aleksandr <sanek20062006@mail.ru>
2020-04-03 15:19:07 +0300

    Create Task33

--- Lessons/src/main/java/by/home/Lesson14/Task33/Main.java
+++ Lessons/src/main/java/by/home/Lesson14/Task33/Main.java
@@ -1,37 +1,48 @@
 package by.home.Lesson14.Task33;
 
 import java.io.File;
+import java.util.Objects;
 
 /*Вывести список файлов и каталогов выбранного каталога на диске. Файлы
 и каталоги должны быть распечатаны отдельно.*/
 
 public class Main {
 
-    final static String selectCatalog = "C:\\Total Commander";
+    final static String selectCatalog = "C:\\TEST";
 
     public static void main(String[] args) {
+        System.out.println("\nКаталоги находящиеся по пути: " + selectCatalog);
         printListCatalogs(selectCatalog);
+        System.out.println("\nФайлы находящиеся по пути: " + selectCatalog);
         printListFiles(selectCatalog);
     }
 
     public static void printListCatalogs(String path) {
         File[] listFiles = new File(path).listFiles();
-        System.out.println("\nКаталоги находящиеся по пути: " + path);
         assert listFiles != null;
-        for (File file : listFiles) {
-            if (file.isDirectory()) {
-                System.out.print("[" + file.getName() + "] ");
+
+        for (File fileTemp : listFiles) {
+            if (fileTemp.isDirectory()) {
+                System.out.println("[" + fileTemp.getAbsolutePath() + "] ");
+                printListCatalogs(String.valueOf(fileTemp));
             }
         }
     }
 
     public static void printListFiles(String path) {
         File[] listFiles = new File(path).listFiles();
-        System.out.println("\nФайлы находящиеся в каталоге: " + path);
         assert listFiles != null;
+
         for (File file : listFiles) {
             if (file.isFile()) {
                 System.out.print("[" + file.getName() + "] ");
+            }
+        }
+
+        for (File fileDirectory : listFiles) {
+            if (fileDirectory.isDirectory()){
+                System.out.println("\nФайлы находящиеся в подкаталоге: " + fileDirectory.getAbsolutePath());
+                printListFiles(fileDirectory.getAbsolutePath());
             }
         }
     }
